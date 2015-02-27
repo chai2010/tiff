d6ea34cb1c7f71ee5e39d1246e12d73f7848c23f
chai2010 <chaishushan@gmail.com>
2015-02-27 10:51:24 +0800

    add internal image

--- image.go
+++ image.go
@@ -7,8 +7,53 @@ package tiff
 import (
 	"image"
 	"image/color"
+	"image/draw"
 	"reflect"
 )
+
+var (
+	_ internalImage = (*Gray)(nil)
+	_ internalImage = (*Gray16)(nil)
+	_ internalImage = (*Gray32i)(nil)
+	_ internalImage = (*Gray32f)(nil)
+	_ internalImage = (*Gray64i)(nil)
+	_ internalImage = (*Gray64f)(nil)
+	_ internalImage = (*GrayA)(nil)
+	_ internalImage = (*GrayA32)(nil)
+	_ internalImage = (*GrayA64i)(nil)
+	_ internalImage = (*GrayA64f)(nil)
+	_ internalImage = (*GrayA128i)(nil)
+	_ internalImage = (*GrayA128f)(nil)
+	_ internalImage = (*RGB)(nil)
+	_ internalImage = (*RGB48)(nil)
+	_ internalImage = (*RGB96i)(nil)
+	_ internalImage = (*RGB96f)(nil)
+	_ internalImage = (*RGB192i)(nil)
+	_ internalImage = (*RGB192f)(nil)
+	_ internalImage = (*RGBA)(nil)
+	_ internalImage = (*RGBA64)(nil)
+	_ internalImage = (*RGBA128i)(nil)
+	_ internalImage = (*RGBA128f)(nil)
+	_ internalImage = (*RGBA256i)(nil)
+	_ internalImage = (*RGBA256f)(nil)
+)
+
+type internalImage interface {
+	// Pix holds the image's pixels, as pixel values in big-endian order format. The pixel at
+	// (x, y) starts at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*PixelSize].
+	Pix() []byte
+	// Stride is the Pix stride (in bytes) between vertically adjacent pixels.
+	Stride() int
+	// Rect is the image's bounds.
+	Rect() image.Rectangle
+
+	// 1:Gray, 2:GrayA, 3:RGB, 4:RGBA
+	Channels() int
+	// Uint8/Uint16/Int32/Int64/Float32/Float64
+	Depth() reflect.Kind
+
+	draw.Image
+}
 
 type Image struct {
 	// Pix holds the image's pixels, as pixel values in big-endian order format. The pixel at
--- image_helper.go
+++ image_helper.go
@@ -5,8 +5,9 @@
 package tiff
 
 import (
+	"encoding/binary"
 	"fmt"
-	"image/color"
+	"math"
 	"reflect"
 )
 
@@ -25,62 +26,290 @@ func getPixelSize(channels int, dataType reflect.Kind) int {
 	}
 }
 
-func (p *Image) GrayAt(x, y int) color.Gray {
-	return color.Gray{}
+func pGrayAt(pix []byte) colorGray {
+	return colorGray{
+		Y: pix[1*0],
+	}
 }
-func (p *Image) Gray16At(x, y int) color.Gray16 {
-	return color.Gray16{}
-}
-func (p *Image) Gray32fAt(x, y int) float32 {
-	return 0
-}
-
-func (p *Image) RGBAt(x, y int) [3]uint8 {
-	return [3]uint8{}
-}
-func (p *Image) RGB48At(x, y int) [3]uint16 {
-	return [3]uint16{}
-}
-func (p *Image) RGB96fAt(x, y int) [3]float32 {
-	return [3]float32{}
+func pSetGray(pix []byte, c colorGray) {
+	pix[1*0] = c.Y
 }
 
-func (p *Image) RGBAAt(x, y int) color.RGBA {
-	return color.RGBA{}
+func pGray16At(pix []byte) colorGray16 {
+	return colorGray16{
+		Y: binary.BigEndian.Uint16(pix[2*0:]),
+	}
 }
-func (p *Image) RGBA64At(x, y int) color.RGBA64 {
-	return color.RGBA64{}
-}
-func (p *Image) RGBA128fAt(x, y int) [4]float32 {
-	return [4]float32{}
-}
-
-func (p *Image) SetGray(x, y int, c color.Gray) {
-	//
-}
-func (p *Image) SetGray16(x, y int, c color.Gray16) {
-	//
-}
-func (p *Image) SetGray32f(x, y int, c float32) {
-	//
+func pSetGray16(pix []byte, c colorGray16) {
+	binary.BigEndian.PutUint16(pix[2*0:], c.Y)
 }
 
-func (p *Image) SetRGB(x, y int, c [3]uint8) {
-	//
+func pGray32iAt(pix []byte) colorGray32i {
+	return colorGray32i{
+		Y: int32(binary.BigEndian.Uint32(pix[4*0:])),
+	}
 }
-func (p *Image) SetRGB48(x, y int, c [3]uint16) {
-	//
-}
-func (p *Image) SetRGB96f(x, y int, c [3]float32) {
-	//
+func pSetGray32i(pix []byte, c colorGray32i) {
+	binary.BigEndian.PutUint32(pix[4*0:], uint32(c.Y))
 }
 
-func (p *Image) SetRGBA(x, y int, c color.RGBA) {
-	//
+func pGray32fAt(pix []byte) colorGray32f {
+	return colorGray32f{
+		Y: math.Float32frombits(binary.BigEndian.Uint32(pix[4*0:])),
+	}
 }
-func (p *Image) SetRGBA64(x, y int, c color.RGBA64) {
-	//
+func pSetGray32f(pix []byte, c colorGray32f) {
+	binary.BigEndian.PutUint32(pix[4*0:], math.Float32bits(c.Y))
 }
-func (p *Image) SetRGBA128f(x, y int, c [4]float32) {
-	//
+
+func pGray64iAt(pix []byte) colorGray64i {
+	return colorGray64i{
+		Y: int64(binary.BigEndian.Uint64(pix[8*0:])),
+	}
+}
+func pSetGray64i(pix []byte, c colorGray64i) {
+	binary.BigEndian.PutUint64(pix[8*0:], uint64(c.Y))
+}
+
+func pGray64fAt(pix []byte) colorGray64f {
+	return colorGray64f{
+		Y: math.Float64frombits(binary.BigEndian.Uint64(pix[8*0:])),
+	}
+}
+func pSetGray64f(pix []byte, c colorGray64f) {
+	binary.BigEndian.PutUint64(pix[8*0:], math.Float64bits(c.Y))
+}
+
+func pGrayAAt(pix []byte) colorGrayA {
+	return colorGrayA{
+		Y: pix[1*0],
+		A: pix[1*1],
+	}
+}
+func pSetGrayA(pix []byte, c colorGrayA) {
+	pix[1*0] = c.Y
+	pix[1*1] = c.A
+}
+
+func pGrayA32At(pix []byte) colorGrayA32 {
+	return colorGrayA32{
+		Y: binary.BigEndian.Uint16(pix[2*0:]),
+		A: binary.BigEndian.Uint16(pix[2*1:]),
+	}
+}
+func pSetGrayA32(pix []byte, c colorGrayA32) {
+	binary.BigEndian.PutUint16(pix[2*0:], c.Y)
+	binary.BigEndian.PutUint16(pix[2*1:], c.A)
+}
+
+func pGrayA64iAt(pix []byte) colorGrayA64i {
+	return colorGrayA64i{
+		Y: int32(binary.BigEndian.Uint32(pix[4*0:])),
+		A: int32(binary.BigEndian.Uint32(pix[4*1:])),
+	}
+}
+func pSetGrayA64i(pix []byte, c colorGrayA64i) {
+	binary.BigEndian.PutUint32(pix[4*0:], uint32(c.Y))
+	binary.BigEndian.PutUint32(pix[4*1:], uint32(c.A))
+}
+
+func pGrayA64fAt(pix []byte) colorGrayA64f {
+	return colorGrayA64f{
+		Y: math.Float32frombits(binary.BigEndian.Uint32(pix[4*0:])),
+		A: math.Float32frombits(binary.BigEndian.Uint32(pix[4*1:])),
+	}
+}
+func pSetGrayA64f(pix []byte, c colorGrayA64f) {
+	binary.BigEndian.PutUint32(pix[4*0:], math.Float32bits(c.Y))
+	binary.BigEndian.PutUint32(pix[4*1:], math.Float32bits(c.A))
+}
+
+func pGrayA128iAt(pix []byte) colorGrayA128i {
+	return colorGrayA128i{
+		Y: int64(binary.BigEndian.Uint64(pix[8*0:])),
+		A: int64(binary.BigEndian.Uint64(pix[8*1:])),
+	}
+}
+func pSetGrayA128i(pix []byte, c colorGrayA128i) {
+	binary.BigEndian.PutUint64(pix[8*0:], uint64(c.Y))
+	binary.BigEndian.PutUint64(pix[8*1:], uint64(c.A))
+}
+
+func pGrayA128fAt(pix []byte) colorGrayA128f {
+	return colorGrayA128f{
+		Y: math.Float64frombits(binary.BigEndian.Uint64(pix[8*0:])),
+		A: math.Float64frombits(binary.BigEndian.Uint64(pix[8*1:])),
+	}
+}
+func pSetGrayA128f(pix []byte, c colorGrayA128f) {
+	binary.BigEndian.PutUint64(pix[8*0:], math.Float64bits(c.Y))
+	binary.BigEndian.PutUint64(pix[8*1:], math.Float64bits(c.A))
+}
+
+func pRGBAt(pix []byte) colorRGB {
+	return colorRGB{
+		R: pix[1*0],
+		G: pix[1*1],
+		B: pix[1*2],
+	}
+}
+func pSetRGB(pix []byte, c colorRGB) {
+	pix[1*0] = c.R
+	pix[1*1] = c.G
+	pix[1*2] = c.B
+}
+
+func pRGB48At(pix []byte) colorRGB48 {
+	return colorRGB48{
+		R: binary.BigEndian.Uint16(pix[2*0:]),
+		G: binary.BigEndian.Uint16(pix[2*1:]),
+		B: binary.BigEndian.Uint16(pix[2*2:]),
+	}
+}
+func pSetRGB48(pix []byte, c colorRGB48) {
+	binary.BigEndian.PutUint16(pix[2*0:], c.R)
+	binary.BigEndian.PutUint16(pix[2*1:], c.G)
+	binary.BigEndian.PutUint16(pix[2*2:], c.B)
+}
+
+func pRGB96iAt(pix []byte) colorRGB96i {
+	return colorRGB96i{
+		R: int32(binary.BigEndian.Uint32(pix[4*0:])),
+		G: int32(binary.BigEndian.Uint32(pix[4*1:])),
+		B: int32(binary.BigEndian.Uint32(pix[4*2:])),
+	}
+}
+func pSetRGB96i(pix []byte, c colorRGB96i) {
+	binary.BigEndian.PutUint32(pix[4*0:], uint32(c.R))
+	binary.BigEndian.PutUint32(pix[4*1:], uint32(c.G))
+	binary.BigEndian.PutUint32(pix[4*2:], uint32(c.B))
+}
+
+func pRGB96fAt(pix []byte) colorRGB96f {
+	return colorRGB96f{
+		R: math.Float32frombits(binary.BigEndian.Uint32(pix[4*0:])),
+		G: math.Float32frombits(binary.BigEndian.Uint32(pix[4*1:])),
+		B: math.Float32frombits(binary.BigEndian.Uint32(pix[4*2:])),
+	}
+}
+func pSetRGB96f(pix []byte, c colorRGB96f) {
+	binary.BigEndian.PutUint32(pix[4*0:], math.Float32bits(c.R))
+	binary.BigEndian.PutUint32(pix[4*1:], math.Float32bits(c.G))
+	binary.BigEndian.PutUint32(pix[4*2:], math.Float32bits(c.B))
+}
+
+func pRGB192iAt(pix []byte) colorRGB192i {
+	return colorRGB192i{
+		R: int64(binary.BigEndian.Uint64(pix[8*0:])),
+		G: int64(binary.BigEndian.Uint64(pix[8*1:])),
+		B: int64(binary.BigEndian.Uint64(pix[8*2:])),
+	}
+}
+func pSetRGB192i(pix []byte, c colorRGB192i) {
+	binary.BigEndian.PutUint64(pix[8*0:], uint64(c.R))
+	binary.BigEndian.PutUint64(pix[8*1:], uint64(c.G))
+	binary.BigEndian.PutUint64(pix[8*2:], uint64(c.B))
+}
+
+func pRGB192fAt(pix []byte) colorRGB192f {
+	return colorRGB192f{
+		R: math.Float64frombits(binary.BigEndian.Uint64(pix[8*0:])),
+		G: math.Float64frombits(binary.BigEndian.Uint64(pix[8*1:])),
+		B: math.Float64frombits(binary.BigEndian.Uint64(pix[8*2:])),
+	}
+}
+func pSetRGB192f(pix []byte, c colorRGB192f) {
+	binary.BigEndian.PutUint64(pix[8*0:], math.Float64bits(c.R))
+	binary.BigEndian.PutUint64(pix[8*1:], math.Float64bits(c.G))
+	binary.BigEndian.PutUint64(pix[8*2:], math.Float64bits(c.B))
+}
+
+func pRGBAAt(pix []byte) colorRGBA {
+	return colorRGBA{
+		R: pix[1*0],
+		G: pix[1*1],
+		B: pix[1*2],
+		A: pix[1*3],
+	}
+}
+func pSetRGBA(pix []byte, c colorRGBA) {
+	pix[1*0] = c.R
+	pix[1*1] = c.G
+	pix[1*2] = c.B
+	pix[1*3] = c.A
+}
+
+func pRGBA64At(pix []byte) colorRGBA64 {
+	return colorRGBA64{
+		R: binary.BigEndian.Uint16(pix[2*0:]),
+		G: binary.BigEndian.Uint16(pix[2*1:]),
+		B: binary.BigEndian.Uint16(pix[2*2:]),
+		A: binary.BigEndian.Uint16(pix[2*3:]),
+	}
+}
+func pSetRGBA64(pix []byte, c colorRGBA64) {
+	binary.BigEndian.PutUint16(pix[2*0:], c.R)
+	binary.BigEndian.PutUint16(pix[2*1:], c.G)
+	binary.BigEndian.PutUint16(pix[2*2:], c.B)
+	binary.BigEndian.PutUint16(pix[2*3:], c.A)
+}
+
+func pRGBA128iAt(pix []byte) colorRGBA128i {
+	return colorRGBA128i{
+		R: int32(binary.BigEndian.Uint32(pix[4*0:])),
+		G: int32(binary.BigEndian.Uint32(pix[4*1:])),
+		B: int32(binary.BigEndian.Uint32(pix[4*2:])),
+		A: int32(binary.BigEndian.Uint32(pix[4*3:])),
+	}
+}
+func pSetRGBA128i(pix []byte, c colorRGBA128i) {
+	binary.BigEndian.PutUint32(pix[4*0:], uint32(c.R))
+	binary.BigEndian.PutUint32(pix[4*1:], uint32(c.G))
+	binary.BigEndian.PutUint32(pix[4*2:], uint32(c.B))
+	binary.BigEndian.PutUint32(pix[4*3:], uint32(c.A))
+}
+
+func pRGBA128fAt(pix []byte) colorRGBA128f {
+	return colorRGBA128f{
+		R: math.Float32frombits(binary.BigEndian.Uint32(pix[4*0:])),
+		G: math.Float32frombits(binary.BigEndian.Uint32(pix[4*1:])),
+		B: math.Float32frombits(binary.BigEndian.Uint32(pix[4*2:])),
+		A: math.Float32frombits(binary.BigEndian.Uint32(pix[4*3:])),
+	}
+}
+func pSetRGBA128f(pix []byte, c colorRGBA128f) {
+	binary.BigEndian.PutUint32(pix[4*0:], math.Float32bits(c.R))
+	binary.BigEndian.PutUint32(pix[4*1:], math.Float32bits(c.G))
+	binary.BigEndian.PutUint32(pix[4*2:], math.Float32bits(c.B))
+	binary.BigEndian.PutUint32(pix[4*3:], math.Float32bits(c.A))
+}
+
+func pRGBA256iAt(pix []byte) colorRGBA256i {
+	return colorRGBA256i{
+		R: int64(binary.BigEndian.Uint64(pix[8*0:])),
+		G: int64(binary.BigEndian.Uint64(pix[8*1:])),
+		B: int64(binary.BigEndian.Uint64(pix[8*2:])),
+		A: int64(binary.BigEndian.Uint64(pix[8*3:])),
+	}
+}
+func pSetRGBA256i(pix []byte, c colorRGBA256i) {
+	binary.BigEndian.PutUint64(pix[8*0:], uint64(c.R))
+	binary.BigEndian.PutUint64(pix[8*1:], uint64(c.G))
+	binary.BigEndian.PutUint64(pix[8*2:], uint64(c.B))
+	binary.BigEndian.PutUint64(pix[8*3:], uint64(c.A))
+}
+
+func pRGBA256fAt(pix []byte) colorRGBA256f {
+	return colorRGBA256f{
+		R: math.Float64frombits(binary.BigEndian.Uint64(pix[8*0:])),
+		G: math.Float64frombits(binary.BigEndian.Uint64(pix[8*1:])),
+		B: math.Float64frombits(binary.BigEndian.Uint64(pix[8*2:])),
+		A: math.Float64frombits(binary.BigEndian.Uint64(pix[8*3:])),
+	}
+}
+func pSetRGBA256f(pix []byte, c colorRGBA256f) {
+	binary.BigEndian.PutUint64(pix[8*0:], math.Float64bits(c.R))
+	binary.BigEndian.PutUint64(pix[8*1:], math.Float64bits(c.G))
+	binary.BigEndian.PutUint64(pix[8*2:], math.Float64bits(c.B))
+	binary.BigEndian.PutUint64(pix[8*3:], math.Float64bits(c.A))
 }
